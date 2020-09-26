f6742d4446f0e83a8ce0a3c0be2794678146b49c
CoderSaanvi <mishra.saanvi@gmail.com>
2020-09-26 13:02:16 -0400

    added infinite lives

--- sketch.js
+++ sketch.js
@@ -27,6 +27,8 @@ function draw() {
   background(0);
   console.log(getFrameRate());
 
+  edges = createEdgeSprites();
+
  // frameRate(1000);
 
   switch(gameState){
@@ -44,7 +46,8 @@ function draw() {
     textSize(40);
     text("Level 1",325,262);
     textSize(35);
-    text("Escape the zombies before all of them take over!",20,320);
+    text("Escape the zombies with limited lives before all",30,320);
+    text("your lives are gone!",268,360);
     //text("only have 3 lives.",270,360);
     textSize(50);
     text("Press Space to Start",167,708);
@@ -88,11 +91,13 @@ function draw() {
     break;
 
     case 2:
-    fill("white");
     textSize(30);
     fill("red");
     text("Level 2",335,262);
-    text("Escape the zombies and now monsters before all of them take over!",10,320);
+    text("Escape the zombies and now monsters with limited lives",30,320);
+    text("without losing all three lives!",230,360);
+    textSize(50);
+    text("Press Space To Go On To Level 2",40,707);
    // text("Remember... you only have 3 minutes and 3 lives.",10,360);
    prince.x = 400;
    prince.y = 455;
@@ -130,13 +135,17 @@ function draw() {
     if(prince.collide(finishLine)){
       gameState = 4;
     }
+    
+break;
 
-    case 4: fill("white");
+    case 4:
     textSize(30);
     fill("green");
     text("Level 3",335,262);
-    text("Escape the zombies and monsters and don't run into the burning hot walls before everything becomes a disatser!",10,320);
-   // text("Remember... you only have 4 minutes and 4 lives. This is the last level!",10,360);
+    text("Escape the zombies and monsters with infinite lives now.",10,320);
+    text("Reach the princess before everything becomes a disaster!",10,360);
+    textSize(50);
+    text("Press Space To Go On To Level 3",40,707);
    prince.x = 400;
    prince.y = 455;
    prince.scale = 1;
@@ -147,14 +156,14 @@ function draw() {
      prince.y = 15;
      prince.scale = 0.23;
    }
-   break;
+    break;
 
-    case 5: background(mazeImg);
+    case 5: background(mazeImg)
 
-    spawnZombies();
-    spawnMonsters();
-      
-    if(keyIsDown(UP_ARROW)){
+      spawnInfiniteZombies();
+      spawnInfiniteMonsters();
+
+      if(keyIsDown(UP_ARROW)){
         prince.y = prince.y-4;
       }
   
@@ -175,9 +184,15 @@ function draw() {
     }
     break;
 
-    case 6: fill("white");
+    case 6: 
+    
+    fill("white");
     textSize(30);
-    text("Congratulations!");
+    text("Congratulations! You saved the princess!");
+
+    if(keyIsDown(32)){
+      gameState = 0;
+    }
   }
 
   drawSprites();
@@ -192,6 +207,12 @@ function draw() {
 function spawnZombies() {
   if(frameCount %40 === 0){
     var zombie = createSprite(random(4,764),random(50,780),10,10);
+
+    zombie.bounceOff(edges[0]);
+    zombie.bounceOff(edges[2]);
+    zombie.bounceOff(edges[3]);
+    zombie.bounceOff(edges[1]);
+
     zombie.addImage(zombieImg);
     zombie.velocityX = random(-2,2);
     zombie.velocityY = random(-2,2);
@@ -203,10 +224,52 @@ function spawnZombies() {
 function spawnMonsters() {
   if(frameCount %40 === 0){
     var monster = createSprite(random(4,764),random(50,780),10,10);
+
+    monster.bounceOff(edges[0]);
+    monster.bounceOff(edges[2]);
+    monster.bounceOff(edges[3]);
+    monster.bounceOff(edges[1]);
+
     monster.addImage(monsterImg);
     monster.velocityX = random(-2,2);
     monster.velocityY = random(-2,2);
     monster.scale = 0.06;
     monster.lifetime = 250;
+  }
+}
+
+function spawnInfiniteZombies() {
+  if(frameCount %50 === 0){
+    edges = createEdgeSprites();
+    var zombies = createSprite(random(4,764),random(50,780),10,10);
+
+    zombies.bounceOff(edges[0]);
+    zombies.bounceOff(edges[2]);
+    zombies.bounceOff(edges[3]);
+    zombies.bounceOff(edges[1]);
+
+    zombies.addImage(zombieImg);
+    zombies.velocityX = random(-2,2);
+    zombies.velocityY = random(-2,2);
+    zombies.scale = 0.06;
+  //  zombies.lifetime = 250;
+  }
+}
+
+function spawnInfiniteMonsters() {
+  if(frameCount %50 === 0){
+    edges = createEdgeSprites();
+    var monsters = createSprite(random(4,764),random(50,780),10,10);
+    
+    monsters.bounceOff(edges[0]);
+    monsters.bounceOff(edges[2]);
+    monsters.bounceOff(edges[3]);
+    monsters.bounceOff(edges[1]);
+
+    monsters.addImage(monsterImg);
+    monsters.velocityX = random(-2,2);
+    monsters.velocityY = random(-2,2);
+    monsters.scale = 0.06;
+   // monsters.lifetime = 250;
   }
 }
