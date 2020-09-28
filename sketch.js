b8b7f17ecc21a2be61fe9beaa330bf1981ba7187
CoderSaanvi <mishra.saanvi@gmail.com>
2020-09-28 16:21:09 -0400

    added the walls to the image

--- sketch.js
+++ sketch.js
@@ -78,7 +78,247 @@ function draw() {
     prince.y = 15;
   }
 
-  if(prince.x > 323 && prince.x <572 && prince.y>172 && prince.y<179){
+  if(prince.x > 324 && prince.x <571 && prince.y>170 && prince.y<180){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 57 && prince.x <161 && prince.y>228 && prince.y<238){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 324 && prince.x <627 && prince.y>228 && prince.y<238){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 9 && prince.x <218 && prince.y>284 && prince.y<291){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 372 && prince.x <469 && prince.y>285 && prince.y<291){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 518 && prince.x <627 && prince.y>284 && prince.y<292){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 56 && prince.x <267 && prince.y>340 && prince.y<348){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 372 && prince.x <522 && prince.y>340 && prince.y<348){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 107 && prince.x <165 && prince.y>400 && prince.y<407){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 323 && prince.x <754 && prince.y>397 && prince.y<405){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 214 && prince.x <756 && prince.y>454 && prince.y<462){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 161 && prince.x <325 && prince.y>509 && prince.y<518){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 375 && prince.x <592 && prince.y>509 && prince.y<518){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 643 && prince.x < 754 && prince.y> 509 && prince.y< 518){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 164 && prince.x < 272 && prince.y> 566 && prince.y<574){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 427 && prince.x < 537 && prince.y> 566 && prince.y< 574){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 706 && prince.x < 821 && prince.y> 566 && prince.y< 574){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 107 && prince.x < 272 && prince.y> 623 && prince.y<630){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 429 && prince.x < 593 && prince.y> 623 && prince.y< 630){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 643 && prince.x < 700 && prince.y> 622 && prince.y< 631){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 55 && prince.x < 326 && prince.y> 678 && prince.y< 686){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 376 && prince.x < 701 && prince.y> 677 && prince.y< 685){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 110 && prince.x < 378 && prince.y> 729 && prince.y< 735){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 437 && prince.x < 759 && prince.y> 728 && prince.y<735){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 56 && prince.x < 759 && prince.y> 776 && prince.y< 781){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 52 && prince.x < 59 && prince.y> 62 && prince.y<233){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 52 && prince.x < 59 && prince.y> 343 && prince.y< 778){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 106 && prince.x < 113 && prince.y> 117 && prince.y< 175){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 106 && prince.x < 113 && prince.y> 402 && prince.y< 628){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 160 && prince.x < 165 && prince.y> 117 && prince.y< 233){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 160 && prince.x < 165 && prince.y> 401 && prince.y< 516){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 212 && prince.x < 219 && prince.y> 59 && prince.y< 288){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 212 && prince.x < 219 && prince.y> 400 && prince.y< 459){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 266 && prince.x < 273 && prince.y> 60 && prince.y< 458){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 266 && prince.x < 273 && prince.y> 567 && prince.y< 629){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 323 && prince.x < 330 && prince.y> 121 && prince.y< 177){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 323 && prince.x < 330 && prince.y> 231 && prince.y< 403){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 321 && prince.x < 327 && prince.y> 512 && prince.y< 683){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 371 && prince.x < 378 && prince.y> 284 && prince.y< 345){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 373 && prince.x < 380 && prince.y> 508 && prince.y< 733){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 428 && prince.x < 434 && prince.y> 566 && prince.y< 627){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 515 && prince.x < 520 && prince.y> 284 && prince.y< 347){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 643 && prince.x < 648 && prince.y> 511 && prince.y< 627){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 630 && prince.x < 635 && prince.y> 3 && prince.y< 344){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 682 && prince.x < 687 && prince.y> 60 && prince.y< 345){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 695 && prince.x < 700 && prince.y> 626 && prince.y< 683){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 749 && prince.x < 756 && prince.y> 60 && prince.y< 403){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 749 && prince.x < 756 && prince.y> 455 && prince.y< 516){
+    prince.x = 660;
+    prince.y = 15;
+  }
+
+  if(prince.x > 756 && prince.x < 763 && prince.y> 569 && prince.y< 779){
     prince.x = 660;
     prince.y = 15;
   }
@@ -99,7 +339,7 @@ function draw() {
     textSize(40);
     text("Level 1",325,262);
     textSize(35);
-    text("Escape the zombies with limited lives before all",30,320);
+    text("Escape the mortal zombies before all",100,320);
     text("your lives are gone!",268,360);
     //text("only have 3 lives.",270,360);
     textSize(50);
@@ -163,7 +403,7 @@ function draw() {
     textSize(30);
     fill("red");
     text("Level 2",335,262);
-    text("Escape the zombies and now monsters with limited lives",30,320);
+    text("Escape the mortal zombies and now monsters",100,320);
     text("without losing all three lives!",230,360);
     textSize(50);
     text("Press Space To Go On To Level 2",40,707);
@@ -233,7 +473,7 @@ break;
     textSize(30);
     fill("green");
     text("Level 3",335,262);
-    text("Escape the zombies and monsters with infinite lives now.",10,320);
+    text("Escape the immortal zombies and monsters now.",80,320);
     text("Reach the princess before everything becomes a disaster!",10,360);
     textSize(50);
     text("Press Space To Go On To Level 3",40,707);
@@ -333,7 +573,7 @@ break;
 }
 
 function spawnZombies() {
-  if(frameCount %40 === 0){
+  if(frameCount %30 === 0){
     var zombie = createSprite(random(4,764),random(50,780),10,10);
 
     zombie.addImage(zombieImg);
@@ -347,7 +587,7 @@ function spawnZombies() {
 }
 
 function spawnMonsters() {
-  if(frameCount %40 === 0){
+  if(frameCount %30 === 0){
     var monster = createSprite(random(4,764),random(50,780),10,10);
 
     monster.addImage(monsterImg);
